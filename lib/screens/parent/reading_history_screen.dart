8ce5e687266f68535907def8e909749468a7e36a
Desire Fajuyigbe Lois <ads23b00108y@ait.edu.gh>
2025-10-11 13:45:12 +0000

    history import

--- lib/screens/parent/reading_history_screen.dart
+++ lib/screens/parent/reading_history_screen.dart
@@ -1,5 +1,7 @@
 // File: lib/screens/parent/reading_history_screen.dart
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ReadingHistoryScreen extends StatelessWidget {
   const ReadingHistoryScreen({super.key});
